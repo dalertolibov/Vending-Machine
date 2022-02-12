faecf09b0a254aed785d7fb3696ba2d4264b27db
Douglas Howell <douglashowell2@gmail.com>
2022-02-12 10:06:09 -0500

    Generic Test Classes Created

---
+++ src/test/java/com/techelevator/data/BankAccountTest.java
@@ -0,0 +1,31 @@
+package com.techelevator.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BankAccountTest {
+
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void deposit_money() {
+    }
+
+    @Test
+    public void withdraw_money() {
+    }
+
+    @Test
+    public void balance_in_pennies() {
+    }
+
+    @Test
+    public void balance_to_string() {
+    }
+
+    @Test
+    public void return_change_in_coins() {
+    }
+}
---
+++ src/test/java/com/techelevator/data/InventoryTest.java
@@ -0,0 +1,27 @@
+package com.techelevator.data;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class InventoryTest {
+//Douglas Build Tests
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void vending_machine_stock() {
+    }
+
+    @Test
+    public void subtract_from_inventory() {
+    }
+
+    @Test
+    public void return_current_inventory() {
+    }
+
+    @Test
+    public void return_inventory_map() {
+    }
+}
